25667d1b523566d6e046a0665497908125226412
Study Profile <dee.alves2@gmail.com>
2021-06-21 22:36:09 -0300

    trabalhando com operadores do rxjs

---
+++ src/app/acoes/acoes.component.ts
@@ -0,0 +1,17 @@
+import { Subscription } from 'rxjs';
+import { AcoesService } from './acoes.service';
+import { Acoes } from './modelo/acoes';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-acoes',
+  templateUrl: './acoes.component.html',
+  styleUrls: ['./acoes.component.css'],
+})
+export class AcoesComponent {
+  acoesInput = new FormControl();
+  acoes$ = this.acoesService.getAcoes();
+
+  constructor(private acoesService: AcoesService) { }
+}
---
+++ src/app/acoes/acoes.service.ts
@@ -0,0 +1,32 @@
+import { Acao, AcoesAPI } from './modelo/acoes';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map, pluck, tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AcoesService {
+  constructor(private httpClient: HttpClient) { }
+
+  getAcoes() {
+    return this.httpClient.get<AcoesAPI>('http://localhost:3000/acoes').pipe(
+      tap((valor) => console.log(valor)),
+      pluck('payload'),
+      map((acoes) =>
+        acoes.sort((acaoA, acaoB) => this.ordenaPorCodigo(acaoA, acaoB))
+      )
+    );
+  }
+
+  private ordenaPorCodigo(acaoA: Acao, acaoB: Acao): any {
+    if (acaoA.codigo > acaoB.codigo) {
+      return 1;
+    }
+    else if (acaoA.codigo < acaoB.codigo) {
+      return -1;
+    }
+
+    return 0;
+  }
+}
